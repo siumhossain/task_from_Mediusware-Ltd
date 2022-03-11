00417ae3753f3059e1b2fb3b6f7c0c057cb9d90d
siumhossain <sium.hossain@yahoo.com>
2022-03-12 00:15:37 +0600

    1st

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Variant,Product,ProductImage,ProductVariantPrice,ProductVariant
 # Register your models here.
+
 admin.site.register(Variant)
 admin.site.register(ProductImage)
 admin.site.register(Product)
 admin.site.register(ProductVariantPrice)
 admin.site.register(ProductVariant)
+#just to check out way of organized in database 
